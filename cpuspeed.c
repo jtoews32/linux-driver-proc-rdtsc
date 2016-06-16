ac8dc19fe4f638fb3349aff9d8150e5ad2075100
Jonathan Toews <jtoews@searchyourcloud.com>
2016-06-15 18:40:01 -0700

    modification to cpu speed

--- cpuspeed.c
+++ cpuspeed.c
@@ -11,23 +11,21 @@
 
 MODULE_LICENSE("GPL");
 
-unsigned long long  stamp1;
-unsigned long long  stamp2;
-unsigned long long  cycles;
+unsigned long long  stamp1, stamp2, cycles;
 
 static struct proc_dir_entry* proc;
 
 static ssize_t my_info(struct file *file, char *buf, size_t _len, loff_t *ppos)
 {
 	int	 len = 0;
-	volatile int start = jiffies;
-	volatile int timeout = start + HZ/10 ;
+	int start = jiffies_64;
+	int timeout = start + HZ/10 ;
 
 	// asm(" cli \n rdtsc ");
 	asm("rdtsc");
 	asm(" mov %%eax, stamp1 \n mov %%edx, stamp1+4 " : : : "ax", "dx" );
 
-	while(jiffies < timeout) {} // Go for 1/10 of a second
+	while(jiffies_64 < timeout) {} // Go for 1/10 of a second
 
 	//asm(" rdtsc \n sti ");
 	asm("rdtsc");
@@ -35,11 +33,11 @@ static ssize_t my_info(struct file *file, char *buf, size_t _len, loff_t *ppos)
 
 	cycles = stamp2 - stamp1;
 
-	volatile int finish = jiffies;
+	int finish = jiffies_64;
 
 	len += sprintf( buf+len, "\n Elapsed cpu-cycles = %llu\n", cycles );
 	len += sprintf( buf+len, "\n Jiffies = %i\n", (finish - start) );
-	len += sprintf( buf+len, "\n Elapsed cpu-cycles = %iMhz\n", (int) cycles/100000 );
+	len += sprintf( buf+len, "\n CPU Speed = %iMhz\n", (int) cycles/100000 );
 
 	len += sprintf( buf+len, "\n" );
 
@@ -48,28 +46,22 @@ static ssize_t my_info(struct file *file, char *buf, size_t _len, loff_t *ppos)
 
 static const struct file_operations proc_fops =
 {
- .owner = THIS_MODULE,
- .read  = my_info,
-
+	.owner = THIS_MODULE,
+ 	.read  = my_info,
 };
 
 static int my_init (void)
 {
-
 	proc = proc_create( "cpuspeed",  0777, NULL, &proc_fops );
-
 	if (!proc) {
 		return -ENOMEM;
 	}
-
 	return	0;
-
 }
 
 static void my_exit (void)
 {
 	remove_proc_entry( "cpuspeed", NULL );
-
 }
 
 module_init (my_init);
